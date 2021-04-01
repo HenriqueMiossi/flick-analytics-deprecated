7faf28b6145d5be2a3b25a3ea427480c41b5e099
HenriqueMiossi <henriquemiossi@gmail.com>
2021-04-01 10:19:05 -0300

    Added initial support for icons on bottom nav bar
    Now there is a commented code block with a template for dealing with icons. It should be
    revised later if we should use our own svg icons or the react-native-vector-icons library.

--- src/screens/Application/index.js
+++ src/screens/Application/index.js
@@ -10,11 +10,37 @@ const Tab = createBottomTabNavigator();
 
 export default function Application() {
   return (
-    <Tab.Navigator initialRouteName='Dashboard'>
+    <Tab.Navigator 
+      initialRouteName='Dashboard'
+      
+      // Should uncomment this block later when dealing with style and icons
+      /*
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Dashboard') {
+            iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+          } else if (route.name === 'Search') {
+            iconName = focused ? 'ios-list-box' : 'ios-list';
+          } else if (route.name === 'Bookmarks') {
+            iconName = focused ? 'ios-list-box' : 'ios-list';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'ios-list-box' : 'ios-list';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      */
+
+    >
       <Tab.Screen name="Dashboard" component={Dashboard}/>
-      <Tab.Screen name="Bookmarks" component={Bookmarks}/>
       <Tab.Screen name="Search" component={Search}/>
+      <Tab.Screen name="Bookmarks" component={Bookmarks}/>
       <Tab.Screen name="Settings" component={Settings}/>
     </Tab.Navigator>
   );
 }
+
