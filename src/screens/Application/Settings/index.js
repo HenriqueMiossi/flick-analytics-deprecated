9b30e936a58682260c940e2375c5bd2bf27b353b
HenriqueMiossi <henriquemiossi@gmail.com>
2021-04-04 11:17:32 -0300

    Finished creating the Settings screen items
    Now the Settings screen have it's main items. None of them (buttons/picker/switch) are working,
    but now it should be easier to implement some funcionality in the future.

--- src/screens/Application/Settings/index.js
+++ src/screens/Application/Settings/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import {Text, View, StyleSheet, TouchableOpacity, SafeAreaView} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text, 
+  View, 
+  StyleSheet, 
+  TouchableOpacity, 
+  SafeAreaView,
+  Switch
+} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function Settings({ navigation }) {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
     <LinearGradient 
       style={styles.background} 
@@ -36,32 +45,68 @@ export default function Settings({ navigation }) {
             </View>
           </TouchableOpacity>
 
-          <View style={styles.languageContainer}>
-            <Text>
+          <View style={styles.itemsContainer}>
+            <Text style={styles.text}>
               Idioma
             </Text>
             
             <DropDownPicker
-            
               items={[
                   {label: 'Português', value: 'pt'},
                   {label: 'English', value: 'us'},
                   {label: 'Español', value: 'es'},
               ]}
               defaultValue={'pt'}
-              containerStyle={{width: 200, height: 30}}
-              style={{backgroundColor: '#fafafa'}}
+              containerStyle={{width: 150, height: 40}}
+              style={styles.picker}
               itemStyle={{
-                  justifyContent: 'flex-start',
+                  justifyContent: 'center',
               }}
               dropDownStyle={{backgroundColor: '#fafafa'}}
-              arrowStyle={{marginRight: 10}}
-
-              
+              labelStyle={{
+                fontSize: 16,
+                textAlign: 'center',
+                color: '#000'
+              }}
+              selectedLabelStyle={{
+                fontWeight: 'bold',
+              }}
             />
-
           </View>
+          <View style={styles.separatorLine}/>
 
+          <View style={styles.itemsContainer}>
+            <Text style={styles.text}>
+              Ativar Notificações
+            </Text>
+            <Switch
+              trackColor={{ false: "#B5304C", true: "#31B43F" }}
+              thumbColor={isEnabled ? "#f4f3f4" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+          </View>
+          <View style={styles.separatorLine}/>
+          
+          <View>
+            <TouchableOpacity style={styles.itemsContainer}>
+              <Text style={styles.text}>
+                Reportar um Bug
+              </Text>
+              <Ionicons name={'bug-sharp'} size={24} color={'#B5304C'}/>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.separatorLine}/>
+
+          <View style={styles.itemsContainer}>
+            <Text style={styles.text}>
+              Versão do Aplicativo
+            </Text>
+            <Text style={styles.text}>
+              0.1 - Alpha
+            </Text>
+          </View>
         </View>
 
       </SafeAreaView>
@@ -84,19 +129,16 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     paddingTop: 30
   },
-
   mainContainer: {
     backgroundColor: 'white',
     marginTop: 20,
     marginHorizontal: 10,
     borderRadius: 20,
-    padding: 10,
-    height: 500,
+    paddingVertical: 20,
+    paddingHorizontal: 30,
   },
-
   profileContainer: {
     flexDirection: 'row',
-    justifyContent: 'flex-start',
   },
   textContainer: {
     justifyContent: 'center',
@@ -110,15 +152,26 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginLeft: 'auto',
   },
-
   profileButton: {
     marginBottom: 30,
   },
-
-  languageContainer: {
+  itemsContainer: {
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
-
+  picker: {
+    backgroundColor: '#fafafa',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  separatorLine: {
+    borderBottomColor: '#B5304C',
+    borderBottomWidth: 1,
+    marginVertical: 20
+  },
   background: {
     flex: 1,
   },
