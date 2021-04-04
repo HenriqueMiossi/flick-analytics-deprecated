7469e3f1099b313b329d6b850cce5bdeb50402e9
HenriqueMiossi <henriquemiossi@gmail.com>
2021-04-03 21:31:16 -0300

    Added initial items to settings page
    Now the settings page contains a placeholder user button. This page also have initial support for a dropdown picker
    for selecting the app language. Still needs to improve picker styling and add support for images.

--- src/screens/Application/Settings/index.js
+++ src/screens/Application/Settings/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Text, View, StyleSheet, TouchableOpacity, SafeAreaView} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function Settings({ navigation }) {
   return (
@@ -11,9 +13,57 @@ export default function Settings({ navigation }) {
       end={{x: 1, y: 1}}
     >
       <SafeAreaView>
-        <Text style={styles.text}>
-          Settings
-        </Text>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>
+            Configurações
+          </Text>
+        </View>
+
+
+        <View style={styles.mainContainer}>
+
+          <TouchableOpacity style={styles.profileButton}>
+            <View style={styles.profileContainer}>
+              <Ionicons name={'person-sharp'} size={80} color={'#B5304C'}/>
+
+              <View style={styles.textContainer}>
+                <Text style={styles.profileName}>Username</Text>
+                <Text>Ver informações da conta</Text>
+              </View>
+
+              <Ionicons style={styles.arrowIcon} name={'chevron-forward-sharp'} size={24} color={'black'}/>
+
+            </View>
+          </TouchableOpacity>
+
+          <View style={styles.languageContainer}>
+            <Text>
+              Idioma
+            </Text>
+            
+            <DropDownPicker
+            
+              items={[
+                  {label: 'Português', value: 'pt'},
+                  {label: 'English', value: 'us'},
+                  {label: 'Español', value: 'es'},
+              ]}
+              defaultValue={'pt'}
+              containerStyle={{width: 200, height: 30}}
+              style={{backgroundColor: '#fafafa'}}
+              itemStyle={{
+                  justifyContent: 'flex-start',
+              }}
+              dropDownStyle={{backgroundColor: '#fafafa'}}
+              arrowStyle={{marginRight: 10}}
+
+              
+            />
+
+          </View>
+
+        </View>
+
       </SafeAreaView>
     </LinearGradient>
   );
@@ -21,29 +71,55 @@ export default function Settings({ navigation }) {
 
 const styles = StyleSheet.create({
   title: {
-    fontSize: 36,
-    textAlign: 'center',
+    fontSize: 30,
+    textAlign: 'left',
     paddingBottom: 20,
-    color: 'white',
+    color: '#B5304C',
     fontWeight: 'bold',
-    marginBottom: 50,
   },
-  text: {
-    fontSize: 24,
+  titleContainer: {
+    backgroundColor: 'white',
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+    paddingHorizontal: 30,
+    paddingTop: 30
+  },
+
+  mainContainer: {
+    backgroundColor: 'white',
+    marginTop: 20,
+    marginHorizontal: 10,
+    borderRadius: 20,
+    padding: 10,
+    height: 500,
+  },
+
+  profileContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  textContainer: {
+    justifyContent: 'center',
+    flexShrink: 1,
+  },
+  profileName: {
+    fontSize: 36,
     fontWeight: 'bold',
-    textAlign: 'center',
-    color: 'white',
   },
+  arrowIcon: {
+    alignSelf: 'center',
+    marginLeft: 'auto',
+  },
+
+  profileButton: {
+    marginBottom: 30,
+  },
+
+  languageContainer: {
+    flexDirection: 'row',
+  },
+
   background: {
     flex: 1,
-    justifyContent: 'center',
-  },
-  button: {
-    backgroundColor: 'white',
-    padding: 10,
-    margin: 20,
-    borderRadius: 40,
-    height: 60,
-    justifyContent: 'center',
   },
 });
